c05de01975bbc27a75c7959a0e2bab56cdc900fb
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-04-07 16:52:05 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  getForm();
+
 });
+
+
+
+function getForm() {
+  let f = document.getElementById('create-task-form');
+  let v = document.getElementById('new-task-description');
+
+  f.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let u = document.getElementById('tasks');
+    u.innerHTML += `<li>${v.value}</li>`;
+    v.value = "";
+    // alert("test");
+  });
+}
