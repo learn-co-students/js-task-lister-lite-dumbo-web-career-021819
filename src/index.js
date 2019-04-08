5b98a6b01fe1531bed0d453137f68165e5ededd6
cstruong <christophertruongm@gmail.com>
2019-04-08 17:17:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const formTag = document.body.querySelector('form#create-task-form');
+toDoUlTag = document.body.querySelector('ul#tasks');
+
+formTag.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputTag = event.target.querySelector('input#new-task-description');
+  createNewLi(inputTag.value);
+  inputTag.value = '';
+})
+
+toDoUlTag.addEventListener('click', event => {
+  if (event.target.className === 'deleteButton') {
+    event.target.parentElement.remove();
+  }
+})
+
 });
+
+
+const createNewLi = (eventValue) => {
+  toDoUlTag.innerHTML +=  `<li>${eventValue} <button class="deleteButton">X</button></li>`;
+}
