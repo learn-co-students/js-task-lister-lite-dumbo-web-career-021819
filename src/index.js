5db3e231f2e29337e72c6a40358e21259d2a7d2e
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-04-09 00:38:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// Tags
+const formTag = document.querySelector('form#create-task-form');
+const ulTag = document.querySelector('ul#tasks');
+
+
+// Helper Functions
+const createListItem = task => {
+  return `<li>${task} <button class="delete">❌</button></li>`;
+};
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  formTag.addEventListener('submit', event => {
+    event.preventDefault();
+    let task = document.getElementById('new-task-description').value;
+    ulTag.innerHTML += createListItem(task);
+  });
+
+  ulTag.addEventListener('click', event => {
+    if (event.target.className === "delete") {
+      let currentDeleteBtn = event.target;
+      currentDeleteBtn.parentElement.remove();
+    }
+  });
+
 });
