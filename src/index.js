23d84c4b3af950ee3319d2132e5b05f673f9e755
merestrickland <mere.strickland@gmail.com>
2019-04-10 17:17:38 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  // - users should be able to type a task into the input field
+  // - users should be able to click some form of a submit button
+  // - the task string that the user provided should appear on the DOM after the submit button has been activated
+
+
+
+  const formTag = document.getElementById("create-task-form")
+
+  const inputField = document.getElementById("new-task-description")
+
+  const ulTag = document.getElementById("tasks")
+  // debugger;
+
+  formTag.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newTaskText = inputField.value;
+
+    ulTag.innerHTML +=
+      `<li> ${newTaskText} </li>`
+
+  })
+  // console.log(inputField.name)
+  // debugger;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
