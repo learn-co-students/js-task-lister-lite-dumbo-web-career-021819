828d20424147b5f0acd2aaa622ef90ff2a64d28b
github username <github email address>
2019-04-11 18:06:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let tasksUlTag = document.querySelector('#tasks')
+  let formTag = document.querySelector('form')
+  let taskLiTag = (task) => {
+    return `<li>${task}
+    <button>X</button
+    </li>`
+  }
+
+  formTag.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let submitButton = document.querySelector('.submit-btn')
+    if ( event.target.lastElementChild === submitButton) {
+      let newTask = document.querySelector('#new-task-description').value
+      tasksUlTag.innerHTML += taskLiTag(newTask)
+      taskLiTag.addEventListener('click', (event) => {
+        debugger
+      })
+    }
+  })
 });
